04c19f864228de271f5d0a370312f15a1d928065
Leonardo Salas <leosalass@gmail.com>
2017-09-29 15:00:21 -0500

    Nuevo: - agreado seeder de usuarios para el usuario administrador

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
         $this->call(UserStatesTableSeeder::class);
+        $this->call(UserPermissionsTableSeeder::class);
+        $this->call(UserRolesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
